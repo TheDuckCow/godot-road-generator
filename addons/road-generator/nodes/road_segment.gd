f7e59603e25e96e48a34492f33da7917e964051c
Patrick W. Crawford <theduckcow@live.com>
2024-02-20 23:23:21 -0800

    Applying static typing assignments to curve offset algorithm

--- addons/road-generator/nodes/road_segment.gd
+++ addons/road-generator/nodes/road_segment.gd
@@ -11,6 +11,7 @@ extends Spatial
 # const RoadSegment = preload("res://addons/road-generator/road_segment.gd")
 
 const LOWPOLY_FACTOR = 3.0
+const RAD_NINETY_DEG = PI/2 # aka 1.5707963267949, used for offset_curve algorithm
 
 signal seg_ready(road_segment)
 
@@ -308,50 +309,51 @@ func generate_lane_segments(_debug: bool = false) -> bool:
 
 
 ## Offsets a destination curve from a source curve by a specified distance.
-#  Evaluates 4 points on source curve: Point 0 and 1 positions as well as
-#  point-0-out and point-1-in handles. Requires transforms for point 0
-#  and point 1, which determine the direction of the handles. Calculates best
-#  fit position for destination curve given the supplied curves, transforms,
-#  and distance.
+##
+##  Evaluates 4 points on source curve: Point 0 and 1 positions as well as
+##  point-0-out and point-1-in handles. Requires transforms for point 0
+##  and point 1, which determine the direction of the handles. Calculates best
+##  fit position for destination curve given the supplied curves, transforms,
+##  and distance.
 func offset_curve(road_seg: Spatial, road_lane: Path, in_offset: float, out_offset: float, rp0: Spatial, rp1: Spatial):
 
-	var src = road_seg.curve
-	var dst = road_lane.curve
-	var a_gbasis = rp0.global_transform.basis
-	var d_gbasis = rp1.global_transform.basis
-	var in_pos = start_point.global_transform.origin + (a_gbasis.x * in_offset * _start_flip_mult)
-	var out_pos = end_point.global_transform.origin + (d_gbasis.x * out_offset * _end_flip_mult)
+	var src:Curve3D = road_seg.curve
+	var dst:Curve3D = road_lane.curve
+	var a_gbasis := rp0.global_transform.basis
+	var d_gbasis := rp1.global_transform.basis
+	var in_pos := start_point.global_transform.origin + (a_gbasis.x * in_offset * _start_flip_mult)
+	var out_pos := end_point.global_transform.origin + (d_gbasis.x * out_offset * _end_flip_mult)
 
 	# Get initial point locations
-	var pt_a = to_global(src.get_point_position(0))
-	var pt_b = to_global(src.get_point_position(0) + src.get_point_out(0))
-	var pt_c = to_global(src.get_point_position(1) + src.get_point_in(1))
-	var pt_d = to_global(src.get_point_position(1))
+	var pt_a := to_global(src.get_point_position(0))
+	var pt_b := to_global(src.get_point_position(0) + src.get_point_out(0))
+	var pt_c := to_global(src.get_point_position(1) + src.get_point_in(1))
+	var pt_d := to_global(src.get_point_position(1))
 
 	# TTD: Project point(s) onto plane(s)
 
 	# Project primary curve points to secondary curve points
-	var pt_e = pt_a + (a_gbasis.x * in_offset)
-	var pt_i = pt_b + (a_gbasis.x * in_offset)
-	var pt_h = pt_d + (d_gbasis.x * out_offset)
-	var pt_j = pt_c + (d_gbasis.x * out_offset)
+	var pt_e := pt_a + (a_gbasis.x * in_offset)
+	var pt_i := pt_b + (a_gbasis.x * in_offset)
+	var pt_h := pt_d + (d_gbasis.x * out_offset)
+	var pt_j := pt_c + (d_gbasis.x * out_offset)
 
 	# Get vectors from points
-	var vec_ab = pt_b - pt_a
-	var vec_bc = pt_c - pt_b
-	var vec_cd = pt_d - pt_c
+	var vec_ab := pt_b - pt_a
+	var vec_bc := pt_c - pt_b
+	var vec_cd := pt_d - pt_c
 
 	# Calculate secondary curve handles and setup curves
-	var angle_q = -vec_ab.signed_angle_to(vec_bc, a_gbasis.y) * 0.5
-	var angle_s = vec_cd.signed_angle_to(vec_bc, d_gbasis.y) * 0.5
-	var offset_q = tan(angle_q) * in_offset
-	var offset_s = tan(angle_s) * out_offset
-	var pt_f = a_gbasis.z * (vec_ab.length() + offset_q)
-	var pt_g = -d_gbasis.z * (vec_cd.length() + offset_s)
-	var rad_ninety_deg = 1.5707963267949 # 90 degrees in radians
-	var margin = 0.1745329 # Margin to check above/below 90. 0.174 is roughly 10 degrees
+	var angle_q := -vec_ab.signed_angle_to(vec_bc, a_gbasis.y) * 0.5
+	var angle_s := vec_cd.signed_angle_to(vec_bc, d_gbasis.y) * 0.5
+	var offset_q := tan(angle_q) * in_offset
+	var offset_s := tan(angle_s) * out_offset
+	var pt_f := a_gbasis.z * (vec_ab.length() + offset_q)
+	var pt_g := -d_gbasis.z * (vec_cd.length() + offset_s)
 
-	if abs(angle_q) > rad_ninety_deg - margin and abs(angle_q) < rad_ninety_deg + margin:
+	var margin := 0.1745329 # Margin to check above/below 90. 0.174 is roughly 10 degrees
+
+	if abs(angle_q) > RAD_NINETY_DEG - margin and abs(angle_q) < RAD_NINETY_DEG + margin:
 		# Angle is close to 90deg. Use default values.
 		dst.add_point(
 			road_lane.to_local(in_pos),
@@ -364,7 +366,7 @@ func offset_curve(road_seg: Spatial, road_lane: Path, in_offset: float, out_offs
 			road_lane.to_local(to_global(src.get_point_in(0))),
 			road_lane.to_local(to_global(pt_f)))
 
-	if abs(angle_s) > rad_ninety_deg - margin and abs(angle_s) < rad_ninety_deg + margin:
+	if abs(angle_s) > RAD_NINETY_DEG - margin and abs(angle_s) < RAD_NINETY_DEG + margin:
 		# Angle is close to 90deg. Use default values.
 		dst.add_point(
 			road_lane.to_local(out_pos),
