a647bc2756eedfab3de371c55e81fcd012a50a68
leems-dreems <130364182+leems-dreems@users.noreply.github.com>
2025-07-16 12:09:34 +0100

    Astar demo: code cleanup and comments

--- demo/navigation/astar_lane_graph.gd
+++ demo/navigation/astar_lane_graph.gd
@@ -1,11 +1,11 @@
+## Demo scene showing pathfinding over RoadLanes using an AStar3D graph. Left-click to set path start, right-click to set path end.
 extends Node3D
 
-@export var path_marker_mesh: Mesh
-@export var path_highlight_material: Material
+@export var path_marker_mesh: Mesh ## Mesh used to mark AStar points along RoadLanes
+@export var path_highlight_material: Material ## Material used for markers along the current path
 
-const search_radius_squared := 100.0 ## Pathfinding will find the nearest graph point, and then try out other points within this radius (squared)
+const search_radius_squared := 100.0 ## Pathfinding will find the nearest graph point, and then try out other points within the square root of this radius
 const astar_point_interval := 10.0 ## Distance between astar points along road lanes
-var lanes_by_id: Dictionary
 var endpoints_dict: Dictionary = {} ## Used to track the astar indices of the start & end points of each RoadLane
 var id_path: PackedInt64Array ## AStar point IDs for the most recently plotted route
 
@@ -13,6 +13,7 @@ var id_path: PackedInt64Array ## AStar point IDs for the most recently plotted r
 @onready var path_start_marker: MeshInstance3D = $PathStartMarker
 @onready var path_end_marker: MeshInstance3D = $PathEndMarker
 @onready var astar := AStar3D.new()
+
 
 func _ready() -> void:
 	var _road_lanes: Array[Node] = find_children("*", "RoadLane", true, false)
@@ -30,21 +31,18 @@ func _ready() -> void:
 
 			var _marker := MeshInstance3D.new()
 			_marker.mesh = path_marker_mesh
-			_marker.add_to_group("AStarPathDot")
 			marker_container.add_child(_marker)
 			_marker.global_position = _road_lane.to_global(_lane_points[_index])
 			_marker.set_meta("point_id", _new_id)
 
-			lanes_by_id[_new_id] = _road_lane
-
 			if _index == 0: # If this is the first point on this lane, store its index
 				endpoints_dict[_new_id] = _road_lane
-			elif _index > 0: # Make a one-way connection from the previous point to this one
+			else: # Make a one-way connection from the previous point to this one
 				astar.connect_points(_previous_point_id, _new_id, false)
 			_previous_point_id = _new_id
 			_index += _increment_amount
 
-		# Add astar point for end of lane's curve
+		# Add an astar point at the end of the lane's curve
 		var _endpoint_id := astar.get_available_point_id()
 		astar.add_point(_endpoint_id, _road_lane.to_global(_lane_points[len(_lane_points) - 1]))
 		astar.connect_points(_previous_point_id, _endpoint_id, false)
@@ -53,15 +51,13 @@ func _ready() -> void:
 		_end_marker.mesh = path_marker_mesh
 		marker_container.add_child(_end_marker)
 		_end_marker.global_position = _road_lane.to_global(_lane_points[len(_lane_points) - 1])
-		_end_marker.add_to_group("AStarPathDot")
 		_end_marker.set_meta("point_id", _endpoint_id)
-		lanes_by_id[_endpoint_id] = _road_lane
 
-	for _point_idx: int in endpoints_dict.keys():
+	for _point_idx: int in endpoints_dict.keys(): # Loop over endpoints and connect lanes to each other
 		var _point_position := astar.get_point_position(_point_idx)
 		var _lane: RoadLane = endpoints_dict[_point_idx]
 		if _point_position == _lane.to_global(_lane.curve.get_point_position(0)):
-			var _overlapping_indices := endpoints_dict.keys().filter(func(i):
+			var _overlapping_indices := endpoints_dict.keys().filter(func(i): # Find other endpoints close to this endpoint
 				var _other_point_position := astar.get_point_position(i)
 				var _other_lane: RoadLane = endpoints_dict[i]
 				if _other_point_position != _other_lane.to_global(_other_lane.curve.get_point_position(_other_lane.curve.point_count - 1)):
@@ -83,7 +79,7 @@ func _unhandled_input(event: InputEvent) -> void:
 		_ray_params.to = _ray_params.from + $Camera3D.project_ray_normal(get_viewport().get_mouse_position()) * $Camera3D.far
 
 		var _result = _space_state.intersect_ray(_ray_params)
-		if _result.size() > 0:
+		if _result.size() > 0: # Move our "start marker" to the clicked location, and plot a route to the end marker
 			path_start_marker.visible = true
 			path_start_marker.position = _result.position
 			path_start_marker.position.y += 2
@@ -96,14 +92,14 @@ func _unhandled_input(event: InputEvent) -> void:
 		_ray_params.to = _ray_params.from + $Camera3D.project_ray_normal(get_viewport().get_mouse_position()) * $Camera3D.far
 
 		var _result = _space_state.intersect_ray(_ray_params)
-		if _result.size() > 0:
+		if _result.size() > 0: # Move our "end marker" to the clicked location, and plot a route from the start marker
 			path_end_marker.visible = true
 			path_end_marker.position = _result.position
 			path_end_marker.position.y += 2
 			id_path = plot_route()
 	return
 
-
+## Get an array of astar point ids, representing a path through our astar graph
 func plot_route() -> PackedInt64Array:
 	var _possible_starts: PackedInt64Array
 	var _possible_ends: PackedInt64Array
@@ -139,12 +135,7 @@ func plot_route() -> PackedInt64Array:
 	prints("Path cost:", str(roundi(_lowest_cost)))
 	return _id_path
 
-
-func clear_route() -> void:
-	id_path.clear()
-	return
-
-
+## Calculate the cost of a given astar path
 func get_path_cost(_id_path: PackedInt64Array) -> float:
 	var _path_cost := 0.0
 	var i: int = 0
