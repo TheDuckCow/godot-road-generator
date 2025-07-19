330a968d07c0f3ddb9825f6ce90e66a6f9d01236
Anton Kurkin <kurkin.anton@gmail.com>
2025-07-20 00:22:57 +0200

    fix spawner and make IDM behave reasonable when moving in reverse

--- demo/procedural_generator/road_actor.gd
+++ demo/procedural_generator/road_actor.gd
@@ -18,7 +18,7 @@ enum DriveState {
 @export var visualize_lane := false
 @export var rotate_to_distance := 0.5 # How many meters in front of agent to seek rotation
 @export var auto_register: bool = true
-@export var keep_distance := 2.0
+@export var keep_distance := 0.5
 @export var safe_headway := 1.5 # (secs)
 @export var looking_forward := 50.0
 
@@ -40,7 +40,7 @@ func _ready() -> void:
 		forward_speed = randf_range(forward_speed_min, forward_speed_max)
 	agent.visualize_lane = visualize_lane
 	agent.agent_pos.node = self
-	agent.agent_pos.end_offsets = [2.0, 2.0]
+	agent.agent_pos.end_offsets = [2.5, 2.5]
 	if DEBUG_OUT:
 		print("Agent state: %s par, %s lane (%s offset), %s manager" % [
 			agent.actor, agent.agent_pos.lane if agent.agent_pos else null, agent.agent_pos.offset if agent.agent_pos else NAN, agent.road_manager
@@ -61,33 +61,35 @@ func get_input() -> Vector3:
 		_:
 			return Vector3.ZERO
 
-
 ## For more info see Intelligent driver model
 ## https://en.wikipedia.org/wiki/Intelligent_driver_model
-func compute_idm_acceleration(target_speed: float, accel: float, follow: bool) -> float:
-	assert(target_speed != 0)
+func _compute_idm_acceleration() -> float:
 	const delta_exp := 4.0 # constant emulating acceleration/braking profile
 	var speed := self.get_signed_speed() # if delta_exp is changed from even, make speed abs
-	var dyn_accel := accel * (1 - pow(speed / target_speed, delta_exp))
-	if follow:
-		var vo_pair = agent.find_obstacle(looking_forward, RoadLaneAgent.MoveDir.FORWARD)
-		var forward_distance: float = vo_pair[0]
-		var forward_obstacle: RoadLane.Obstacle = vo_pair[1]
-		if forward_obstacle:
-			assert(forward_distance >= 0)
-			var speed_lead: float = forward_obstacle.speed
-			var gap := forward_distance - keep_distance
+	var vo_pair = agent.find_obstacle(looking_forward, RoadLaneAgent.MoveDir.FORWARD)
+	var forward_distance: float = vo_pair[0]
+	var forward_obstacle: RoadLane.Obstacle = vo_pair[1]
+	var target_speed := forward_speed
+	var accela := acceleration
+	var breaka := breaking
+	var dyn_accel = accela * (1 - pow(speed / target_speed, delta_exp))
+	var reversed_move := speed < 0
+	if reversed_move:
+		dyn_accel = acceleration
+		accela = breaking
+		breaka = acceleration
+	if forward_obstacle:
+		assert(forward_distance >= 0)
+		var speed_lead: float = forward_obstacle.speed
+		var gap := forward_distance - keep_distance
+		if gap <= 0.0:
+			dyn_accel = -breaka
+		else:
 			var s_star := ( keep_distance + speed * safe_headway +
-				(speed * (speed - speed_lead)) / (2 * sqrt(accel * breaking)) )
-			dyn_accel -= accel * pow(s_star / max(gap, 0.1), 2)
-	dyn_accel = clamp(dyn_accel, -breaking, acceleration)
+				(speed * (speed - speed_lead)) / (2 * sqrt(accela * breaka)) )
+			dyn_accel -= accela * sign(s_star) * pow(s_star / gap, 2)
+	dyn_accel = clamp(dyn_accel, -accela, breaka)
 	return dyn_accel
-
-
-func player_breaking(decel: float) -> float:
-	const speed_coeff = 2.0 # without it breaking on speed close to maximum is too slow
-	return compute_idm_acceleration(forward_speed * speed_coeff, decel, false)
-
 
 func _get_auto_input() -> Vector3:
 	if ! agent.is_lane_position_valid():
@@ -101,11 +103,19 @@ func _get_auto_input() -> Vector3:
 			lane_move -= 1
 		elif (cur_cars_r >= 0) && (cur_cars - cur_cars_r > lane_change_tolerance):
 			lane_move += 1
-	var dyn_accel := compute_idm_acceleration(forward_speed, acceleration, true)
+	var dyn_accel := _compute_idm_acceleration()
 
 	return Vector3(lane_move, 0, dyn_accel)
 	#return Vector3(0, 0, dyn_accel)
 
+func _compute_player_acceleration(target_speed: float, accel: float) -> float:
+	const delta_exp := 4.0 # constant emulating acceleration/braking profile
+	# if delta_exp is changed from even, make speed abs
+	return accel * (1 - pow(self.get_signed_speed() / target_speed, delta_exp))
+
+func _compute_player_breaking(decel: float) -> float:
+	const speed_coeff = 2.0 # without it breaking on speed close to maximum is too slow
+	return sign(self.get_signed_speed()) * _compute_player_acceleration(forward_speed * speed_coeff, decel)
 
 func _get_player_input() -> Vector3:
 	if ! agent.is_lane_position_valid():
@@ -118,15 +128,15 @@ func _get_player_input() -> Vector3:
 	var down := Input.is_action_pressed("ui_down")
 	if up == down:
 		if speed != 0:
-			dyn_accel -= sign(speed) * player_breaking(breaking / 2.0)
+			dyn_accel -= _compute_player_breaking(breaking / 2.0)
 	elif up || down:
-		var reversed_move: bool = speed < 0 #accelerate from low negative speeds
+		var reversed_move := speed < 0
 		if speed != 0 && up == reversed_move:
-			dyn_accel -= sign(speed) * player_breaking(breaking)
+			dyn_accel -= _compute_player_breaking(breaking)
 		elif up:
-			dyn_accel += compute_idm_acceleration(forward_speed, acceleration, false)
+			dyn_accel += _compute_player_acceleration(forward_speed, acceleration)
 		elif down:
-			dyn_accel -= compute_idm_acceleration(reverse_speed, acceleration, false)
+			dyn_accel -= _compute_player_acceleration(-reverse_speed, acceleration)
 
 	var lane_move:int = 0
 	if Input.is_action_just_pressed("ui_left"):
@@ -135,12 +145,22 @@ func _get_player_input() -> Vector3:
 		lane_move += 1
 	return Vector3(lane_move, 0, dyn_accel)
 
+func _collide_with(other) -> void:
+		var elasticity := 1.2 # 1.0 - fully elastic, 0.0 - fully inelastic; 1.2 just for fun
+		var self_mass := 1.0
+		var other_mass := 1.0
+		var self_speed := self.velocity.z
+		var other_speed: float = other.velocity.z
+		self.velocity.z = 0#((self_mass - elasticity*other_mass)*self_speed + (1 + elasticity)*other_mass*other_speed) / (self_mass + other_mass)
+		#other.velocity.z = ((other_mass - elasticity*self_mass)*other_speed + (1 + elasticity)*self_mass*self_speed) / (self_mass + other_mass)
 
 func _physics_process(delta: float) -> void:
 	velocity.y = 0
 	var target_dir:Vector3 = get_input()
 	velocity.z -= delta * target_dir.z
 	velocity.z = clamp(velocity.z, -forward_speed * 2, reverse_speed * 2)
+	if abs(velocity.z) < 0.025:
+		velocity.z = 0
 
 	agent.agent_pos.speed = self.get_signed_speed()
 
@@ -164,14 +184,7 @@ func _physics_process(delta: float) -> void:
 	var next_pos: Vector3 = agent.move_along_lane(move_dist)
 	global_transform.origin = next_pos # has to set it before switching lanes
 	if agent.agent_move.block == RoadLaneAgent.MoveBlock.OBSTACLE:
-		var other := agent.agent_move.obstacle.node
-		var elasticity := 1.5 # more than fully elastic (1.0) just for the fun of it
-		var self_mass := 1.0
-		var other_mass := 1.0
-		var self_speed := self.velocity.z
-		var other_speed: float = other.velocity.z
-		self.velocity.z = ((self_mass - elasticity*other_mass)*self_speed + (1 + elasticity)*other_mass*other_speed) / (self_mass + other_mass)
-		other.velocity.z = ((other_mass - elasticity*self_mass)*other_speed + (1 + elasticity)*self_mass*self_speed) / (self_mass + other_mass)
+		_collide_with(agent.agent_move.obstacle.node)
 	elif agent.agent_move.block == RoadLaneAgent.MoveBlock.NO_LANE:
 		var dir: RoadLane.MoveDir = RoadLane.MoveDir.FORWARD if move_dist > 0 else RoadLane.MoveDir.BACKWARD
 		var shared_part := agent.agent_pos.lane.shared_parts[dir]
