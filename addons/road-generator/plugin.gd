d944bcb136d475bd7344810bd64b93bfd9e9d132
Patrick W. Crawford <theduckcow@live.com>
2026-06-12 00:22:15 -0700

    WIP export of gLTF updates

--- addons/road-generator/plugin.gd
+++ addons/road-generator/plugin.gd
@@ -2022,14 +2022,14 @@ func _export_mesh_modal() -> void:
 		return
 	
 	var basepath: String
-	var rc_is_root: bool = false
+	#var rc_is_root: bool = false
 	if selected.owner and selected.owner.scene_file_path:
 		# RC is a child of another editor node
 		var subpath := selected.get_owner().scene_file_path
 		basepath = subpath.get_basename() + "_"
 	elif selected.scene_file_path:
 		# Root of saved save
-		rc_is_root = true
+		#rc_is_root = true
 		var subpath := selected.scene_file_path
 		basepath = subpath.get_basename() + "_"
 	else:
@@ -2053,15 +2053,15 @@ func _export_mesh_modal() -> void:
 	# and there's not the bug of clicking into a folder clearing the filename.
 	_export_file_dialog.access = EditorFileDialog.ACCESS_FILESYSTEM
 
-	if not rc_is_root:
-		# This is the code which disables the ability to instance a glb back
-		# into the scene when the RoadContainer is the root. This is because
-		# it is reliably crashing Godot for some reason, so we don't want to
-		# let it to users while it is unstable.
-		_export_file_dialog.set_option_count(1)
-		_export_file_dialog.set_option_name(0, "Instance after export")
-		_export_file_dialog.set_option_values(0, ["Yes", "No"])
-		_export_file_dialog.set_option_default(0, 1)
+	# if not rc_is_root:
+	# 	# This is the code which disables the ability to instance a glb back
+	# 	# into the scene when the RoadContainer is the root. This is because
+	# 	# it is reliably crashing Godot for some reason, so we don't want to
+	# 	# let it to users while it is unstable.
+	# 	_export_file_dialog.set_option_count(1)
+	# 	_export_file_dialog.set_option_name(0, "Instance after export")
+	# 	_export_file_dialog.set_option_values(0, ["Yes", "No"])
+	# 	_export_file_dialog.set_option_default(0, 1)
 		
 	_export_file_dialog.file_selected.connect(_export_gltf)
 	_export_file_dialog.popup_centered_ratio()
@@ -2100,8 +2100,10 @@ func _export_gltf(path: String) -> void:
 			meshes.append(intersec._mesh.mesh)
 			unset_owners.append([intersec._mesh, intersec._mesh.owner])
 			intersec._mesh.owner = owner
-		
 	
+	print("_prompt_instance_gltf -1")
+	await EditorInterface.get_base_control().get_tree().process_frame
+	print("_prompt_instance_gltf 0")
 	var gltf_document_save := GLTFDocument.new()
 	var gltf_state_save := GLTFState.new()
 
@@ -2115,29 +2117,63 @@ func _export_gltf(path: String) -> void:
 	for unsetter in unset_owners:
 		unsetter[0].owner = unsetter[1]
 
-	if instance_after_export:
-		_instance_gltf_post_export(container, path)
+	var local_path := ProjectSettings.localize_path(path)
+	var is_in_project := local_path.begins_with("res://")
+	print("_prompt_instance_gltf 1")
+	if is_in_project:
+		await EditorInterface.get_base_control().get_tree().process_frame
+		_prompt_instance_gltf(container, local_path)
 	else:
 		EditorInterface.get_resource_filesystem().scan_sources()
 	
 	_export_file_dialog.queue_free()
 
 
-func _instance_gltf_post_export(container:RoadContainer, export_file: String) -> void:
-	var local_path := ProjectSettings.localize_path(export_file)
-	if export_file == local_path and not export_file.begins_with("res://"):
-		push_error("Failed to localize the path, ensure gltf was saved within project folder to instance after")
-		return
-	
+func _prompt_instance_gltf(container: RoadContainer, local_path: String) -> void:	
+	print("_prompt_instance_gltf 2")
 	var fs := EditorInterface.get_resource_filesystem()
-	fs.update_file(local_path)
+	#fs.update_file(local_path)
+	#fs.scan_sources()
 	fs.scan_sources()
+	# Wait for scan to complete
+	print("_prompt_instance_gltf 3")
+	#await fs.resources_reload
+	await fs.resources_reimported
+	print("_prompt_instance_gltf 4")
 	await EditorInterface.get_base_control().get_tree().process_frame
+	print("_prompt_instance_gltf 5")
+	while fs.is_scanning():
+		await EditorInterface.get_base_control().get_tree().process_frame
+		print("_prompt_instance_gltf 6")
+
+	print("_prompt_instance_gltf 7")
+
+	var confirm_dialog := ConfirmationDialog.new()
+	confirm_dialog.title = "Instance exported gLTF?"
+	confirm_dialog.dialog_text = "The gLTF file export was successful. Instance it now?\nThis will disable Create Geo on the RoadContainer."
+	confirm_dialog.get_ok_button().text = "Yes"
+	confirm_dialog.get_cancel_button().text = "No"
+	
+	confirm_dialog.confirmed.connect(func():
+		_instance_gltf_in_place(container, local_path)
+		confirm_dialog.queue_free()
+	)
+	confirm_dialog.canceled.connect(func():
+		confirm_dialog.queue_free()
+	)
+	
+	EditorInterface.get_base_control().add_child(confirm_dialog)
+	confirm_dialog.popup_centered()
+	print("_prompt_instance_gltf 8")
+
+
+func _instance_gltf_in_place(container:RoadContainer, export_file: String) -> void:
+	print("_instance_gltf_in_place 1")
 	var glb_scene:PackedScene = load(export_file)
 	if not glb_scene:
 		push_error("Failed load gltf/glb export, check output path and try again")
 		return
-	
+	print("_instance_gltf_in_place 2")
 	var glb_model:Node3D = glb_scene.instantiate()
 	glb_model.name = export_file.get_file().get_basename()
 	
